0a2a578d0baae8cea391cb283e5c73fcbf516f60
DHANA LAKSHMI <66015455+thunder-07@users.noreply.github.com>
2022-03-06 23:23:45 +0530

    Update models.py

--- loginit/models.py
+++ loginit/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class saver(models.Model):
     username = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
     password = models.CharField(max_length=50)
     def __str__(self):
         return self.username
